47bcb0cd77e8d4e34c805c092281b801a5c68383
riazonbin <riazonbin@mail.ru>
2023-02-04 10:28:59 +0300

    added btn prefab

--- Assets/Core/DefaultQuestions.cs
+++ Assets/Core/DefaultQuestions.cs
@@ -12,7 +12,9 @@ namespace Assets.Core
         public static List<Question> Questions = new()
         {
         new Question{ Description = "Кто поцарапал Сережу?", Answer = "Кот"},
-        new Question{ Description = "Кто купил сайфуллина?", Answer = "Шарипов"},
+        new Question{ Description = "Самая большая страна в мире?", Answer = "Россия"},
+        new Question{ Description = "Крупнейший мировой материк?", Answer = "Евразия"},
+        new Question{ Description = "Язык, на котором говорит больше всего людей в мире?", Answer = "Китайский"},
         new Question{Description = "Глава октябрьской революции?", Answer = "Ленин"},
         new Question{Description = "Кто доказал, что земля круглая?", Answer = "Аристотель"}
         };
