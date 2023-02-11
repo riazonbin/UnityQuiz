cce610769077a9bc308a48264f6230f936553bb1
riazonbin <riazonbin@mail.ru>
2023-02-11 11:05:31 +0300

    making next question transition

--- Assets/scripts/QuestionScript.cs
+++ Assets/scripts/QuestionScript.cs
@@ -11,12 +11,13 @@ public class QuestionScript : MonoBehaviour
     public GameObject text;
     public GameCreation gameCreation;
     public Transform keyboardPanel;
+    public Game game;
     public Stack<Button> QuestionsStack { get; set; } = new Stack<Button>();
 
     // Start is called before the first frame update
     void Start()
     {
-        Game game = gameCreation.game;
+        game = gameCreation.game;
 
         //var question = game[Random.Range(0, Answers.Count - 1)];
         text.GetComponent<Text>().text = game.currentQuestion.Description;
@@ -31,7 +32,11 @@ public class QuestionScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        var test = game.currentQuestion;
+        if (panel.GetComponentsInChildren<Button>().All(x => x.GetComponentInChildren<Text>().text != ""))
+        {
+            NextQuestion();
+        }
     }
 
     public void ReturnButton(Button sender)
@@ -46,5 +51,32 @@ public class QuestionScript : MonoBehaviour
             btn.interactable = true;
         }
         catch { }
+    }
+
+    public void NextQuestion()
+    {
+        string answer = "";
+
+        foreach (Button child in panel.GetComponentsInChildren<Button>())
+        {
+            answer += child.GetComponentInChildren<Text>().text;
+        }
+
+        if (game.CheckWord(answer))
+        {
+            game.NextQuestion();
+            text.GetComponent<Text>().text = game.currentQuestion.Description;
+
+            foreach (Button child in panel.GetComponentsInChildren<Button>())
+            {
+                Destroy(child.gameObject);
+            }
+
+            for (int i = 0; i < game.currentQuestion.Answer.Length; i++)
+            {
+                var btn = Instantiate(preFab, panel);
+                btn.GetComponent<Button>().onClick.AddListener(() => ReturnButton(btn.GetComponent<Button>()));
+            }
+        }
     }
 }
